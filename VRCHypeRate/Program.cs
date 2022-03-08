d7f85b865da42934e520dcd72398f32735619293
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-03-08 05:58:08 +0000

    Add guard against no config file

--- VRCHypeRate/Program.cs
+++ VRCHypeRate/Program.cs
@@ -1,15 +1,25 @@
 ﻿using Newtonsoft.Json;
 using VRCHypeRate.Client;
 using VRCHypeRate.Models;
+using VRCHypeRate.Utils;
 
 namespace VRCHypeRate;
 
 public static class Program
 {
-    public static ConfigModel Config;
+    private static readonly Logger Logger = Logger.GetLogger("VRCHypeRate");
+    public static ConfigModel Config = null!;
     public static void Main()
     {
-        Config = JsonConvert.DeserializeObject<ConfigModel>(File.ReadAllText("./config.json"))!;
+        var config = JsonConvert.DeserializeObject<ConfigModel>(File.ReadAllText("./config.json"));
+        if (config == null)
+        {
+            Logger.Log("Please define a config (config.json)");
+            return;
+        }
+
+        Config = config;
+
         var client = new HypeRateClient(Config.Id, Config.ApiKey);
         client.Connect();
     }
