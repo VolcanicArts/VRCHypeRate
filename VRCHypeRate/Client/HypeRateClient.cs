c38f362cc06d66ad5cd2cedb60cd824346da1bc1
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-03-08 20:29:11 +0000

    Make Logger static

--- VRCHypeRate/Client/HypeRateClient.cs
+++ VRCHypeRate/Client/HypeRateClient.cs
@@ -23,7 +23,7 @@ public class HypeRateClient
     private UdpClient oscClient = null!;
     private Timer heartBeatTimer = null!;
     private bool IsRunning = true;
-    private readonly Logger Logger = Logger.GetLogger(nameof(HypeRateClient));
+    private static readonly Logger Logger = Logger.GetLogger(nameof(HypeRateClient));
 
     public HypeRateClient(string Id, string ApiKey)
     {
