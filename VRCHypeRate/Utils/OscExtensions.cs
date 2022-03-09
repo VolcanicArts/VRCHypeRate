ec3b1a13441e434bddf7a58ba869743488f27167
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-03-09 03:09:10 +0000

    Create extension method for sending parameters

---
+++ VRCHypeRate/Utils/OscExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+using CoreOSC;
+using CoreOSC.IO;
+using VRCHypeRate.Client;
+
+namespace VRCHypeRate.Utils;
+
+public static class OscExtensions
+{
+    public static void sendParameter(this UdpClient oscClient, OSCParameter parameter, object value)
+    {
+        Logger.Log($"Sending parameter {parameter.ToString()} of value {value}", LogLevel.Debug);
+        var message = new OscMessage(new Address($"/avatar/parameters/{parameter.ToString()}"), new[] { value });
+        oscClient.SendMessageAsync(message);
+    }
+}
